aeff2fbb3331582c001b30a108a97d39f4657cc3
rolwyn <rolwyn.quadras051@gmail.com>
2024-04-13 11:36:13 -0400

    Added basic components

---
+++ foodapp/src/Components/Homepage/Homepage.js
@@ -0,0 +1,13 @@
+import Header from '../Header/Header.js';
+import ImageContentWrapper from '../ImageContent/ImageContentWrapper.js';
+
+function Homepage() {
+  return (
+    <div>
+        <Header></Header>
+        <ImageContentWrapper></ImageContentWrapper>
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ foodapp/src/Components/ImageContent/ImageContentWrapper.js
@@ -0,0 +1,11 @@
+
+
+function ImageContentWrapper() {
+  return (
+    <div>
+        <p>Image content here</p>
+    </div>
+  );
+}
+
+export default ImageContentWrapper;
